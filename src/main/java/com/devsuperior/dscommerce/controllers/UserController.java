53d1c8fd48b3ad0c8adae3adc7940777e57ed1aa
Obatista02 <guilherme.santos201412@gmail.com>
2024-11-05 20:19:32 -0300

    added version ProductMinDTO

--- src/main/java/com/devsuperior/dscommerce/controllers/UserController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.devsuperior.dscommerce.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -14,7 +15,7 @@ public class UserController {
 
 	@Autowired
 	private UserService service;
-
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
 	@GetMapping(value = "/me")
 	public ResponseEntity<UserDTO> getMe() {
 		UserDTO dto = service.getMe();
